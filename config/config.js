28d0bc893b554fa9ea9f9f1a4f8ef775b84b730b
Riza Fahmi <rizafahmi@gmail.com>
2017-02-10 16:38:08 +0700

    Sequelize init

---
+++ config/config.js
@@ -0,0 +1,24 @@
+exports.default = 
+{
+  "development": {
+    "username": "postgres",
+    "password": null,
+    "database": "startup_quotes",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": null,
+    "database": "startup_quotes_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DATABASE,
+    "host": process.env.RDS_HOST,
+    "dialect": "postgres"
+  }
+}
