b0cc33fc9d1154fe8dfc213dfd8ed57b0ffa9852
Riza Fahmi <rizafahmi@gmail.com>
2017-02-11 15:51:17 +0700

    Add seeders

---
+++ seeders/20170211154706-seed-quotes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('newquotes', [{
+      quote: 'Make Money or make love',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkInsert('newquotes', null, {})
+  }
+};
