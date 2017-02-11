6994a6b5921aa5d7c78571d32943f85f29bd6c61
Riza Fahmi <rizafahmi@gmail.com>
2017-02-11 11:12:56 +0700

    Print out sequelize object

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports =
   "production": {
     "username": process.env.RDS_USERNAME || null,
     "password": process.env.RDS_PASSWORD || null,
-    "database": "startup_quotes" || null,
+    "database": "startup_quotes",
     "host": process.env.RDS_HOSTNAME || null,
     "port": process.env.RDS_PORT || null,
     "dialect": "postgres"
